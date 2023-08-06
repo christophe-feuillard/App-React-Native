0b56f234d64b6b539d73acc6c5eae37410a00ddf
christophef <christophe.feuillard78320@gmail.com>
2023-08-06 18:14:07 +0200

    bottom navigation bar OK

--- project/App.js
+++ project/App.js
@@ -2,49 +2,54 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
+import MapView from 'react-native-maps';
+
+import MainContainer from './navigation/MainContainer';
 
 export default function App() {
 
-  const [latitude, setLatitude] = useState("")
-  const [longitude, setLongitude] = useState("")
+  // const [latitude, setLatitude] = useState("")
+  // const [longitude, setLongitude] = useState("")
 
-  useEffect(() => {
-    (async() => {
+  // useEffect(() => {
+  //   (async() => {
 
-      let { status } = await Location.requestForegroundPermissionsAsync()
+  //     let { status } = await Location.requestForegroundPermissionsAsync()
 
-      if (status == 'granted') {
-        console.log('permission succesful')
-      } else {
-        console.log('permi non autorisé')
-      }
+  //     if (status == 'granted') {
+  //       console.log('permission succesful')
+  //     } else {
+  //       console.log('permi non autorisé')
+  //     }
 
-      const position = await Location.getCurrentPositionAsync()
-      console.log(position)
+  //     const position = await Location.getCurrentPositionAsync()
+  //     console.log(position)
 
-      setLatitude(position.coords.latitude)
-      setLongitude(position.coords.longitude)
-      // console.log(position.coords.latitude)
-      // console.log(position.coords.longitude)
+  //     setLatitude(position.coords.latitude)
+  //     setLongitude(position.coords.longitude)
+  //     // console.log(position.coords.latitude)
+  //     // console.log(position.coords.longitude)
 
-    })()
+  //   })()
 
-  }, [])
+  // }, [])
   return (
-    <View style={styles.container}>
-      <Text>test</Text>
-      <Text>{latitude}</Text>
-      <Text>{longitude}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MainContainer/>
+    // <View style={styles.container}>
+    //   <Text>test</Text>
+    //   <Text>{latitude}</Text>
+    //   <Text>{longitude}</Text>
+    //   <StatusBar style="auto" />
+    //   <MapView showsUserLocation={true} style={{height: '50%', width: '100%'}} /> 
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
